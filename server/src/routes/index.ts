e01b4f4edf084cc522143418ac15c04ae47ef2b6
Gerry Marie Livergood <83831486+gerry650@users.noreply.github.com>
2023-10-08 00:46:28 -0400

    Update index.ts

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,12 +1,1 @@
-import { Router } from 'express';
-
-import userRouter from './user';
-import keysRouter from './keys';
-
-const router = Router();
-
-router.use('/user', userRouter);
-router.use('/keys', keysRouter);
-
-
-export default router;
+*
