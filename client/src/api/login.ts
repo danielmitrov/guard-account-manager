0b4f8899ae0b4de459eab5613a1d6f2e280430c6
Gerry Marie Livergood <83831486+gerry650@users.noreply.github.com>
2023-10-08 00:57:30 -0400

    Update login.ts

--- client/src/api/login.ts
+++ client/src/api/login.ts
@@ -1,8 +1,1 @@
-import {API} from './index';
-
-
-export async function sendCredentials(credentials: {username: string, password: string}) {
-    const response = await API.post('/user/login', credentials);
-
-    return response.data;
-}
+*
