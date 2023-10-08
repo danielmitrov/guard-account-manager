6f839c51dee203a09c5abe19c54983dceda310ec
Gerry Marie Livergood <83831486+gerry650@users.noreply.github.com>
2023-10-08 01:04:34 -0400

    Update index.tsx

--- client/src/components/InvalidCredentials/index.tsx
+++ client/src/components/InvalidCredentials/index.tsx
@@ -1,10 +1,1 @@
-import React from 'react';
-
-import classes from './InvalidCredentials.module.less';
-
-
-export default function InvalidCredentials() {
-    return <div className={classes.container}>
-        Wrong credentials.
-    </div>
-}
+*
