9589d7b0c5d6276598345ed769d2d2bcb40d3b11
Gerry Marie Livergood <83831486+gerry650@users.noreply.github.com>
2023-10-08 01:02:59 -0400

    Update index.tsx

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,11 +1,1 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import App from './App';
-
-import './index.less';
-
-
-const mountNode = document.getElementById('app');
-const app = React.createElement(App);
-ReactDOM.render(app, mountNode);
+*
