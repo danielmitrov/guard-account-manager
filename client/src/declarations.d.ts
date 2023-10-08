6934620e7b397c6468e8abe9d2969c978af5387f
Gerry Marie Livergood <83831486+gerry650@users.noreply.github.com>
2023-10-08 01:03:32 -0400

    Update declarations.d.ts

--- client/src/declarations.d.ts
+++ client/src/declarations.d.ts
@@ -1,12 +1,1 @@
-declare module '*.module.less' {
-    const classes: {
-        [className: string]: string;
-    };
-    export default classes;
-}
-
-declare module '*.svg';
-declare module '*.png';
-
-// globals
-declare const API_URL: string;
+*
